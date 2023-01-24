60b8644404fa6ab7e9f658ef452b001ffe6fc5aa
Katie Oates <katie.s.oates@gmail.com>
2023-01-23 20:22:22 -1000

    changed menu screen

--- src/styles/MenuStyles.ts
+++ src/styles/MenuStyles.ts
@@ -4,7 +4,7 @@ export default class MenuStyles {
     //background image
     static background = {
         key: 'background',
-        url: 'https://picsum.photos/800/600',
+        url: 'https://th.bing.com/th/id/R.dfdd8b8e762018542fd7141532c95699?rik=5O1NsZt8PboI0w&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fDMJO5GW.jpg&ehk=cmHHaC4jkyWV5HbN5WVc7ywPzlK7d%2ff3ujM56Vkv8Ak%3d&risl=&pid=ImgRaw&r=0',
     }
 
     //title
