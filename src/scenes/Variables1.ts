0adfa3a38bfa5183cfcdd5264231d18712444430
kamtan-bit <kamtan@udel.edu>
2023-01-28 03:21:14 -1000

    tried to make gh-pages and failed

--- src/scenes/Variables1.ts
+++ src/scenes/Variables1.ts
@@ -156,13 +156,15 @@ export default class Variables1 extends Phaser.Scene{
         this.scoreboard = new ScoreDisplay(this, 15, 80)
 
 
+        var zone = this.add.zone(75, 300, 100, 100).setRectangleDropZone(125, 100);
 
-        this.input.on('drop',  (_pointer: any, gameObject: any, _dropZone: any) => {
+        this.input.on('drop',  (_pointer: any, gameObject: any, dropZone: any) => {
 
             this.mi.alpha=1;
             this.pbf.alpha=1;
             this.rbf.alpha=1;
 
+        if (dropZone === zone) {
             switch(gameObject.texture.key) {
                 case 'Moorish Idol':
                     this.pond.setTexture('MI Pond')
@@ -183,6 +185,7 @@ export default class Variables1 extends Phaser.Scene{
                     this.pond.setTexture('Pond')
                     break;
             }
+        }
 
             gameObject.alpha=0.35;
             
