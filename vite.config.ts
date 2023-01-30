1c939a2fd867535a5b620793fbb8d77cb30c66b4
Ethan <113044582+eorev@users.noreply.github.com>
2023-01-30 21:31:55 +0000

    Deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
 	plugins: [],
 	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
-	base: '/https://eorev.github.io/WhatsThePointer/',
+	base: '/WhatsThePointer/',
 })
